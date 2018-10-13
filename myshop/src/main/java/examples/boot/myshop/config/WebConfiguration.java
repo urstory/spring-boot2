97f2830693e97d99f52a0ecee9633323a2c25764
urstory <urstory@gmail.com>
2018-10-13 17:36:32 +0900

    인터셉터 추가함

---
+++ myshop/src/main/java/examples/boot/myshop/config/WebConfiguration.java
@@ -0,0 +1,40 @@
+package examples.boot.myshop.config;
+
+import examples.boot.myshop.utils.MyInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+// WebMvcConfigurer 를 구현하는 클래스는 Spring MVC 를 확장할 수 있는
+// 기능을 가진다.
+@Configuration
+public class WebConfiguration implements WebMvcConfigurer {
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new MyInterceptor());
+    }
+}
+
+
+/*
+
+JDK 8 부터는 default 메소드라는 기능이 추가됨.
+인터페이스도 메소드를 구현할 수 있다.
+
+interface가 있고, 해당 interface를 구현해야 한다.
+이 인터페이스의 모든 기능은 구현할 필요가 없고, 필요한 기능만 구현하고 싶다면 어떻게 할까?
+
+예를 들면 사용자가 MyInterface 라는 interface를 만들었다. 해당 인터페이스가 메소드를
+5개 가지고 있다.
+
+이런 불편함을 해결하려고 MyInterface를 구현한 MyAdapter라는 클래스를 제공한다.
+구현하고 있지만 내용은 없다.
+
+class MyAdapter implements MyInterface{
+ // 5개의 메소드를 구현...
+}
+
+class MyBean extends MyAdapter{
+    // 필요한것만 오버라이딩한다.
+}
+ */
---
+++ myshop/src/main/java/examples/boot/myshop/utils/MyInterceptor.java
@@ -0,0 +1,22 @@
+package examples.boot.myshop.utils;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class MyInterceptor extends HandlerInterceptorAdapter {
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        request.setAttribute("start", System.currentTimeMillis());
+        return true;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+        long end = System.currentTimeMillis();
+        long start = (long)request.getAttribute("start");
+        System.out.println("걸린 시간 :"  + (end - start));
+    }
+}
