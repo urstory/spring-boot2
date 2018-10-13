b0335edfa8e6694f347814704bdc3f342e63a467
urstory <urstory@gmail.com>
2018-10-13 15:47:44 +0900

    파라미터

--- myshop/src/main/java/examples/boot/myshop/controller/BoardController.java
+++ myshop/src/main/java/examples/boot/myshop/controller/BoardController.java
@@ -3,14 +3,27 @@ package examples.boot.myshop.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+// http://localhost:8080/boards?page=2
 @Controller
 @RequestMapping("/boards")
 public class BoardController {
-    //  GET /boartds
-    @GetMapping
-    public String list(){
 
+    //  GET /boards - 1page 의 결과를 달라.
+    //  GET /boards?page=2   - 2page의 결과를 달라.
+    //  GET /boards?page=3   - 3page의 결과를 달라.
+    //  파라미터 searchKind
+    //  파라미터 searchStr  2개의 파라미터는 반드시 필요한 것은 아니다. String type
+    @GetMapping
+    public String list(
+            @RequestParam(name = "page", required = false, defaultValue = "1") int page,
+            @RequestParam(name = "searchKind", required = false) String searchKind,
+            @RequestParam(name = "searchStr", required = false) String searchStr
+    ){
+        System.out.println("page : " + page);
+        System.out.println("searchKind : " + searchKind);
+        System.out.println("searchStr : " + searchStr);
         return "list"; // view name 을 리턴한다.
     }
 }
