b2bb0f45347ae2cb8f27ec3d00a10067b208bae5
urstory <urstory@gmail.com>
2018-10-27 16:01:31 +0900

    UserDetailsService구현

---
+++ myshop/src/main/java/examples/boot/myshop/security/LoginUser.java
@@ -0,0 +1,31 @@
+package examples.boot.myshop.security;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+
+import java.util.Collection;
+
+// spring security가 제공하는 User에 필드를 추가한다.
+public class LoginUser extends User {
+    private String name;
+    private Long id;
+    public LoginUser(String email, String password, Collection<? extends GrantedAuthority> authorities) {
+        super(email, password, true, true, true, true, authorities);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
--- myshop/src/main/java/examples/boot/myshop/security/MyShopUserDetailsService.java
+++ myshop/src/main/java/examples/boot/myshop/security/MyShopUserDetailsService.java
@@ -1,14 +1,44 @@
 package examples.boot.myshop.security;
 
+import examples.boot.myshop.entity.Member;
+import examples.boot.myshop.entity.Role;
+import examples.boot.myshop.repository.MemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
 
 @Component
 public class MyShopUserDetailsService implements UserDetailsService {
+    @Autowired
+    MemberRepository memberRepository;
+
     @Override
-    public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
-        return null;
+    @Transactional(readOnly = true)
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+
+        Member member = memberRepository.getMemberAndRoles(email);
+        if(member == null){
+            throw new UsernameNotFoundException(email + " is not found");
+        }
+        String password = member.getPassword();
+        Collection<GrantedAuthority> authorities = new HashSet<>();
+        Set<Role> roles = member.getRoles();
+        for(Role role:roles) {
+            authorities.add(new SimpleGrantedAuthority("ROLE_" + role.getName()));
+        }
+        LoginUser loginUser = new LoginUser(email, password, authorities);
+        loginUser.setId(member.getId());
+        loginUser.setName(member.getName());
+        return loginUser;
     }
 }
